6193fdf53ec6a543f3c39247c7c89cc0a3a3b531
Anchal935 <80518872+Anchal935@users.noreply.github.com>
2023-11-28 01:06:47 +0530

    Create Exit_Button.c

---
+++ Exit_Button.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+//=====all variables declaration====
+int t_cost;
+int bus_no;
+int num_of_seats;
+int seat_num[50];
+
+// function for exit button
+void exit_button()
+{
+    printf("Thanks to visit our site :)");
+}
