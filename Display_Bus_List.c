995a0348bbc2870e4ef9d75656c881146437e18c
Anchal935 <80518872+Anchal935@users.noreply.github.com>
2023-11-28 00:47:56 +0530

    Create Display_Bus_List.c

---
+++ Display_Bus_List.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+//=====all variables declaration====
+int t_cost;
+int bus_no;
+int num_of_seats;
+int seat_num[50];
+
+// function for displaying bus list 
+void display_bus_list(void)
+{
+    printf("--------------------------------------------------------------------------------------------\n");
+    printf("S.No    BUS NUMBER    BUS NAME               ROUTES                   DATE       TIME        CHARGES\n");
+    printf("--------------------------------------------------------------------------------------------\n");
+    printf("1       1001        Bedi Travels         Delhi To Dharamshala       09/01/2024   6:00 PM      Rs.500\n");
+    printf("2       1002        Ganga Travels        Delhi To Haridwar          10/10/2023   4:00 PM      Rs.250\n");
+    printf("3       1003        Sai Baba Travels     Delhi To Shirdi            09/11/2023   10:00 AM     Rs.600\n");
+    printf("4       1004        Mayur Travels        Ghaziabad To Saharanpur    05/11/2023   7:00 PM      Rs.200\n");
+    printf("5       1005        Shree Travels        Ghaziabad To Meerut        09/12/2023   6:00 AM      Rs.150\n");
+    printf("6       1006        Shiv Ganga Travels   Delhi To Rishikesh         01/10/2023   3:00 PM      Rs.200\n");
+    printf("7       1007        Super Deluxe         Saharanpur To Delhi        13/11/2023   9:00 AM      Rs.300\n");
+    printf("8       1008        Shine On Travels     Saharanpur To Meerut       15/12/2023   2:00 PM      Rs.50\n");
+    printf("9       1009        Shree Travels        Dehradun To Saharanpur     16/11/2023   10:00 AM     Rs.800\n");
+    printf("10      1010        Rajjo Travels        Dehradun To Mumbai         31/12/2023   11:00 AM     Rs.900\n");
+
+}
